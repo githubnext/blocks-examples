423592a8de124474aecafccef3c6c4df8c0f81f2
Irene Alvarado <irealva@github.com>
2021-11-16 11:01:28 -0500

    hook up markdown viewer

---
+++ src/viewers/file-viewers/markdown.tsx
@@ -0,0 +1,10 @@
+import ReactMarkdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
+
+export function Viewer(props: FileViewerProps) {
+  const { content } = props;
+
+  return (
+    <ReactMarkdown children={content} remarkPlugins={[remarkGfm]} />
+  );
+}
