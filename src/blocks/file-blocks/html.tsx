0033186a3c18120d29e38acccf0dd924ca4b84c7
Amelia Wattenbeger <wattenberger@github.com>
2021-11-30 12:13:44 -0500

    add html block

---
+++ src/blocks/file-blocks/html.tsx
@@ -0,0 +1,23 @@
+import { useMemo } from "react";
+import { FileBlockProps } from "@githubnext/utils";
+import DOMPurify from 'dompurify'
+
+export default function (props: FileBlockProps) {
+  const { content } = props;
+
+  const sanitizedContent = useMemo(() => (
+    DOMPurify.sanitize(content)
+  ), [content])
+
+  return (
+    <div style={{
+      padding: "25px 20px"
+    }}>
+      <div style={{
+        all: "initial",
+      }}
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+      />
+    </div>
+  )
+}
