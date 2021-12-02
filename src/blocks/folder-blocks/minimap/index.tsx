9ccc6b3290d7308648a0269e8e5cb0e0f4253e86
Amelia Wattenbeger <wattenberger@github.com>
2021-12-02 11:11:32 -0500

    minimap - use onNavigateToPath prop

--- src/blocks/folder-blocks/minimap/index.tsx
+++ src/blocks/folder-blocks/minimap/index.tsx
@@ -1,10 +1,10 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo } from "react";
 // @ts-ignore
 import { Tree } from "./Tree.jsx"
 import { FolderBlockProps, getNestedFileTree, } from "@githubnext/utils";
 
 export default function (props: FolderBlockProps) {
-  const { tree } = props;
+  const { tree, onNavigateToPath } = props;
 
   const data = useMemo(() => {
     const nestedTree = getNestedFileTree(tree)[0]
@@ -22,12 +22,7 @@ export default function (props: FolderBlockProps) {
       <Tree
         data={data}
         onClickFile={(path: string) => {
-          if ('URLSearchParams' in window) {
-            var searchParams = new URLSearchParams(window.location.search);
-            searchParams.set("path", path);
-            var newRelativePathQuery = window.location.pathname + '?' + searchParams.toString();
-            history.pushState(null, '', newRelativePathQuery);
-          }
+          onNavigateToPath(path);
         }}
       />
     </div>
