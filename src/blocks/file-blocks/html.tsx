26e4b42eb3fc96140fe5889efa131ca0cbb69895
Amelia Wattenbeger <wattenberger@github.com>
2021-12-06 14:36:37 -0500

    html block: don't override entire ALLOWED_TAGS list

--- src/blocks/file-blocks/html.tsx
+++ src/blocks/file-blocks/html.tsx
@@ -6,7 +6,7 @@ export default function (props: FileBlockProps) {
   const { content } = props;
 
   const sanitizedContent = useMemo(() => (
-    DOMPurify.sanitize(content, { ALLOWED_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'mozallowfullscreen', 'webkitallowfullscreen', 'frameborder', 'scrolling', 'width', 'height'] })
+    DOMPurify.sanitize(content, { ADD_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'mozallowfullscreen', 'webkitallowfullscreen', 'frameborder', 'scrolling', 'width', 'height'] })
   ), [content])
 
   return (
