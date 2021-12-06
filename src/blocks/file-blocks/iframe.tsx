ff7a1010cf911550caa6a1fad45dbb4c4cdfe1f4
Irene Alvarado <irealva@github.com>
2021-12-06 12:04:53 -0500

    remove iframe block

--- src/blocks/file-blocks/iframe.tsx
+++
@@ -1,7 +0,0 @@
-import { FileBlockProps } from "@githubnext/utils";
-
-export default function (props: FileBlockProps) {
-    const { content } = props;
-
-    return <div dangerouslySetInnerHTML={{ __html: content }} />
-}
