f35c5216e43676c6d5f047e483cdea82faad3bc8
Matt Rothenberg <mattrothenberg@github.com>
2022-02-08 17:16:11 -0500

    feat: disallow iframes in HTML blocks

--- src/blocks/file-blocks/html.tsx
+++ src/blocks/file-blocks/html.tsx
@@ -1,23 +1,27 @@
 import { useMemo } from "react";
 import { FileBlockProps } from "@githubnext/utils";
-import DOMPurify from 'dompurify'
+import DOMPurify from "dompurify";
 
 export default function (props: FileBlockProps) {
   const { content } = props;
 
-  const sanitizedContent = useMemo(() => (
-    DOMPurify.sanitize(content, { ADD_TAGS: ["iframe"], ADD_ATTR: ['allow', 'allowfullscreen', 'mozallowfullscreen', 'webkitallowfullscreen', 'frameborder', 'scrolling', 'width', 'height'] })
-  ), [content])
+  const sanitizedContent = useMemo(
+    () => DOMPurify.sanitize(content),
+    [content]
+  );
 
   return (
-    <div style={{
-      padding: "25px 20px"
-    }}>
-      <div style={{
-        all: "initial",
+    <div
+      style={{
+        padding: "25px 20px",
       }}
+    >
+      <div
+        style={{
+          all: "initial",
+        }}
         dangerouslySetInnerHTML={{ __html: sanitizedContent }}
       />
     </div>
-  )
+  );
 }
