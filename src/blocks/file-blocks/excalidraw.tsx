7d35cd5ddc6f3ab7cce13750d9410b532cbbd97e
Amelia Wattenberger <wattenberger@github.com>
2022-02-09 10:47:03 -0500

    excalidraw block - use local assets instead of cdn

--- src/blocks/file-blocks/excalidraw.tsx
+++ src/blocks/file-blocks/excalidraw.tsx
@@ -1,12 +1,22 @@
-import Excalidraw, { serializeAsJSON } from "@excalidraw/excalidraw";
 import { FileBlockProps } from "@githubnext/utils";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
+if (typeof window !== "undefined") {
+  // to load assets from self domain, instead of the CDN
+  // @ts-ignore
+  window.EXCALIDRAW_ASSET_PATH = "/excalidraw/"
+}
 export default function (props: FileBlockProps) {
   const { content, onRequestUpdateContent } = props;
 
   const [appState, setAppState] = useState(null);
   const [elements, setElements] = useState([]);
+  const [Excalidraw, setExcalidraw] = useState<any>(null);
+
+  useEffect(() => {
+    import("@excalidraw/excalidraw").then(imp => setExcalidraw(imp.default))
+  }, [])
+  console.log(Excalidraw)
 
   const handleChange = (elements: any, appState: any) => {
     setElements(elements);
@@ -14,15 +24,20 @@ export default function (props: FileBlockProps) {
   };
 
   const handleSave = async () => {
-    if (!serializeAsJSON) return;
-    const serialized = serializeAsJSON(elements, appState);
+    if (!Excalidraw.serializeAsJSON) return;
+    const serialized = Excalidraw.serializeAsJSON(elements, appState);
     onRequestUpdateContent(serialized)
   };
 
   return (
     <div className="position-relative height-full">
       <div className="height-full width-full" key={content}>
-        <Excalidraw initialData={JSON.parse(content)} onChange={handleChange} />
+        {Excalidraw && (
+          <Excalidraw
+            initialData={JSON.parse(content)}
+            onChange={handleChange}
+          />
+        )}
       </div>
       <button className="btn btn-primary position-absolute right-2 top-2"
         style={{ zIndex: 1 }}
