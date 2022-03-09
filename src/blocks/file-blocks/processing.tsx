372c33ac95d40548bc790fa9a984622acfb2ccd3
Irene Alvarado <irealva@github.com>
2022-03-09 11:33:27 -0800

    change processing block to not use eval

--- src/blocks/file-blocks/processing.tsx
+++ src/blocks/file-blocks/processing.tsx
@@ -1,21 +1,64 @@
-import { createRef, useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { FileBlockProps } from "@githubnext/utils";
-import p5 from "p5";
+import DOMPurify from "dompurify";
+
+type LoadingStatus = "ready" | "error" | "idle";
 
 export default function (props: FileBlockProps) {
   const { content } = props;
 
-  const p5Ref = createRef<HTMLDivElement>();
+  const sanitizedContent = useMemo(
+    () => DOMPurify.sanitize(content),
+    [content]
+  );
+
+  // load the p5.js library
+  // this is copied from the way we use to load tailwindcss
+  const src = "https://cdn.jsdelivr.net/npm/p5@1.4.1/lib/p5.js";
+
+  const [status, setStatus] = useState<LoadingStatus>("idle");
 
   useEffect(() => {
-    if (p5Ref.current) {
-      try {
-        new p5(eval(content), p5Ref.current);
-      } catch (e) {
-        // console.log(e);
+    const script = document.createElement("script");
+    script.src = src;
+    script.async = true;
+    script.setAttribute("data-status", "loading");
+    document.body.appendChild(script);
+    const setAttributeFromEvent = (event: any) => {
+      script.setAttribute(
+        "data-status",
+        event.type === "load" ? "ready" : "error"
+      );
+    };
+    script.addEventListener("load", setAttributeFromEvent);
+    script.addEventListener("error", setAttributeFromEvent);
+    const setStateFromEvent = (event: any) => {
+      setStatus(event.type === "load" ? "ready" : "error");
+    };
+    script.addEventListener("load", setStateFromEvent);
+    script.addEventListener("error", setStateFromEvent);
+    return () => {
+      if (script) {
+        script.removeEventListener("load", setStateFromEvent);
+        script.removeEventListener("error", setStateFromEvent);
       }
-    }
-  }, [p5Ref.current, content]);
+    };
+  }, []);
 
-  return <div ref={p5Ref}></div>;
+  // load the p5.js sketch from the content of a user's file
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerText = sanitizedContent;
+    document.body.appendChild(script);
+  }, []);
+
+  return (
+    <div
+      style={{
+        padding: "25px 20px",
+      }}
+    >
+      <div id="processing"></div>
+    </div>
+  );
 }
