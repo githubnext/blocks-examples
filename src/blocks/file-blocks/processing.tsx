0ca083d6960f9db9c98c548e91c6f5acd6968612
Irene Alvarado <irealva@github.com>
2022-03-09 13:53:52 -0800

    add processing block by embedding a sandboxprovider

--- src/blocks/file-blocks/processing.tsx
+++ src/blocks/file-blocks/processing.tsx
@@ -1,64 +1,48 @@
-import { useEffect, useMemo, useState } from "react";
 import { FileBlockProps } from "@githubnext/utils";
-import DOMPurify from "dompurify";
-
-type LoadingStatus = "ready" | "error" | "idle";
+import { SandpackPreview, SandpackProvider } from "@codesandbox/sandpack-react";
 
 export default function (props: FileBlockProps) {
   const { content } = props;
 
-  const sanitizedContent = useMemo(
-    () => DOMPurify.sanitize(content),
-    [content]
-  );
-
-  // load the p5.js library
-  // this is copied from the way we use to load tailwindcss
-  const src = "https://cdn.jsdelivr.net/npm/p5@1.4.1/lib/p5.js";
-
-  const [status, setStatus] = useState<LoadingStatus>("idle");
-
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = src;
-    script.async = true;
-    script.setAttribute("data-status", "loading");
-    document.body.appendChild(script);
-    const setAttributeFromEvent = (event: any) => {
-      script.setAttribute(
-        "data-status",
-        event.type === "load" ? "ready" : "error"
-      );
-    };
-    script.addEventListener("load", setAttributeFromEvent);
-    script.addEventListener("error", setAttributeFromEvent);
-    const setStateFromEvent = (event: any) => {
-      setStatus(event.type === "load" ? "ready" : "error");
-    };
-    script.addEventListener("load", setStateFromEvent);
-    script.addEventListener("error", setStateFromEvent);
-    return () => {
-      if (script) {
-        script.removeEventListener("load", setStateFromEvent);
-        script.removeEventListener("error", setStateFromEvent);
-      }
-    };
-  }, []);
-
-  // load the p5.js sketch from the content of a user's file
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.innerText = sanitizedContent;
-    document.body.appendChild(script);
-  }, []);
-
   return (
     <div
       style={{
-        padding: "25px 20px",
+        width: "100%",
+        height: "100%",
       }}
     >
-      <div id="processing"></div>
+      <SandpackProvider
+        externalResources={[ 'https://cdn.jsdelivr.net/npm/p5@1.4.1/lib/p5.js' ]}
+        customSetup={{
+            files: {
+                "/src/index.js": {
+                  code: content,
+                },
+                "/index.html": {
+                  code: `<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    <main></main>
+    <script src="src/index.js"></script>
+  </body>
+  </html>`
+                }
+              },
+          dependencies: {},
+          entry: "/src/index.js",
+          main: "/src/index.js",
+          environment: "static",
+        }}
+        autorun
+      >
+        <SandpackPreview
+          showOpenInCodeSandbox={false}
+          showRefreshButton={false}
+        />
+      </SandpackProvider>
     </div>
   );
 }
