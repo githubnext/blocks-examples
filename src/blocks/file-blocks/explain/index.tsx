53ee9ceb36e4cc808d6a572d991c0c03c63dff9f
Matt Rothenberg <mattrothenberg@github.com>
2022-04-19 10:41:01 -0400

    fix: more flex tweaks

--- src/blocks/file-blocks/explain/index.tsx
+++ src/blocks/file-blocks/explain/index.tsx
@@ -101,81 +101,74 @@ function BlockInner(props: FileBlockProps) {
   }, [content, context, start, end]);
 
   return (
-    <div className="py-4 h-full explain-block">
-      <div className="max-w-6xl h-full px-4 mx-auto overflow-hidden">
-        <div className="border relative h-full">
-          {start && end && (
-            <div
-              className="z-20 absolute"
-              style={{ top: (start - 1) * 21 - 2, left: -20 }}
-            >
-              <LineMenu
-                start={start}
-                end={end}
-                onCopy={handleCopy}
-                onCopyPermalink={handleCopyPermalink}
-                onExplain={handleExplain}
-              />
-            </div>
-          )}
-          <div className="flex w-full">
-            <div className="flex-1">
-              <SyntaxHighlighter
-                language={
-                  syntaxHighlighterLanguageMap[language] || "javascript"
-                }
-                useInlineStyles={false}
-                wrapLines
-                className="!bg-transparent syntax-highlighter-block overflow-auto"
-                lineProps={(lineNumber) => {
-                  const isHighlighted =
-                    start && end && lineNumber >= start && lineNumber <= end;
-                  return {
-                    "data-highlighted": isHighlighted,
-                    onClick: (
-                      e: React.MouseEvent<HTMLLIElement, MouseEvent>
-                    ) => {
-                      let clicked = e.target as HTMLElement;
-                      if (!clicked.classList.contains("comment")) return;
-                      handleLineClick(lineNumber, e.shiftKey);
-                    },
-                    style: {
-                      display: "block",
-                    },
-                  };
+    <div className="h-full explain-block pl-5 relative">
+      {start && end && (
+        <div
+          className="z-20 absolute"
+          style={{ top: (start - 1) * 18 - 4, left: 8 }}
+        >
+          <LineMenu
+            start={start}
+            end={end}
+            onCopy={handleCopy}
+            onCopyPermalink={handleCopyPermalink}
+            onExplain={handleExplain}
+          />
+        </div>
+      )}
+
+      <div className="flex overflow-auto h-full w-full">
+        <div className="max-w-3xl overflow-auto">
+          <SyntaxHighlighter
+            language={syntaxHighlighterLanguageMap[language] || "javascript"}
+            useInlineStyles={false}
+            wrapLines
+            className="!bg-transparent syntax-highlighter-block"
+            lineProps={(lineNumber) => {
+              const isHighlighted =
+                start && end && lineNumber >= start && lineNumber <= end;
+              return {
+                "data-highlighted": isHighlighted,
+                onClick: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+                  let clicked = e.target as HTMLElement;
+                  if (!clicked.classList.contains("comment")) return;
+                  handleLineClick(lineNumber, e.shiftKey);
+                },
+                style: {
+                  display: "block",
+                },
+              };
+            }}
+            showLineNumbers
+          >
+            {content}
+          </SyntaxHighlighter>
+        </div>
+        <div
+          className="flex-grow-0 flex-shrink-0 relative"
+          style={{ flexBasis: 260 }}
+        >
+          {Object.entries(explanations).map(([line, explanation]) => {
+            return (
+              <div
+                key={line}
+                className="px-3 overflow-y-auto break-all absolute left-0 w-full bg-white"
+                style={{
+                  top: (explanation.start - 1) * 18 - 2,
+                  height: (explanation.end - explanation.start + 1) * 18,
+                  right: 0,
+                  boxShadow: `-2px 0 0 0 #539bf5, inset 1px 0 0 0 #539bf5, -24px 0px 16px -11px rgba(0,0,0,0.05)`,
+                  gridColumn: 1,
+                  gridRow: `${explanation.start}/${explanation.end + 1}`,
                 }}
-                showLineNumbers
               >
-                {content}
-              </SyntaxHighlighter>
-            </div>
-            <div
-              className="flex-grow-0 flex-shrink-0 relative"
-              style={{ flexBasis: 260 }}
-            >
-              {Object.entries(explanations).map(([line, explanation]) => {
-                return (
-                  <div
-                    key={line}
-                    className="px-3 overflow-y-auto break-all absolute left-0 w-full bg-white"
-                    style={{
-                      top: (explanation.start - 1) * 21 - 2,
-                      height: (explanation.end - explanation.start + 1) * 21,
-                      right: 0,
-                      boxShadow: `-2px 0 0 0 #539bf5, inset 1px 0 0 0 #539bf5, -24px 0px 16px -11px rgba(0,0,0,0.05)`,
-                      gridColumn: 1,
-                      gridRow: `${explanation.start}/${explanation.end + 1}`,
-                    }}
-                  >
-                    <ExplanationComponent
-                      language={language}
-                      explanation={explanation}
-                    />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
+                <ExplanationComponent
+                  language={language}
+                  explanation={explanation}
+                />
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
