c57d6236d2cd086054308ba6ca331e771a133cd5
Matt Rothenberg <mattrothenberg@github.com>
2022-04-18 21:33:02 -0400

    feat: use syntax highlighter

--- src/blocks/file-blocks/explain/index.tsx
+++ src/blocks/file-blocks/explain/index.tsx
@@ -1,28 +1,21 @@
 import { FileBlockProps, getLanguageFromFilename } from "@githubnext/utils";
 import { ThemeProvider } from "@primer/react";
 import { useCallback, useState } from "react";
-import { QueryClient, QueryClientProvider, useQuery } from "react-query";
-import { getHighlighter, setCDN } from "shiki";
+import { QueryClient, QueryClientProvider } from "react-query";
+import SyntaxHighlighter from "react-syntax-highlighter";
 import { ExplanationComponent } from "./explanation";
 import "./index.css";
-import { Line } from "./line";
 import { LineMenu } from "./line-menu";
-import { LoadingState } from "./loading-state";
 
-setCDN("https://unpkg.com/shiki/");
+const syntaxHighlighterLanguageMap = {
+  JavaScript: "javascript",
+  TypeScript: "typescript",
+} as Record<string, string>;
 
 export interface Explanation {
   code: string;
   start: number;
   end: number;
-}
-
-async function highlightCode(code: string, language: string) {
-  const highlighter = await getHighlighter({
-    theme: "github-light",
-    langs: ["js", "javascript"],
-  });
-  return highlighter.codeToThemedTokens(code, "js");
 }
 
 function BlockInner(props: FileBlockProps) {
@@ -31,14 +24,6 @@ function BlockInner(props: FileBlockProps) {
   const language = Boolean(context.path)
     ? getLanguageFromFilename(context.path)
     : "N/A";
-
-  const { data, status } = useQuery(
-    ["code", context.file, content],
-    () => highlightCode(content, language),
-    {
-      refetchOnWindowFocus: false,
-    }
-  );
 
   const [start, setStart] = useState<number>();
   const [end, setEnd] = useState<number>();
@@ -71,15 +56,14 @@ function BlockInner(props: FileBlockProps) {
     let startIdx = start;
     let endIdx = end || startIdx;
 
-    const relevantTokens = data?.slice(startIdx - 1, endIdx);
-    if (!relevantTokens) return;
-    const tokensContent = relevantTokens
-      .map((token) => token.map((t) => t.content).join(""))
+    const lineContent = content
+      .split("\n")
+      .slice(startIdx - 1, endIdx)
       .join("\n");
 
     setExplanations((curr) => {
       curr[start] = {
-        code: tokensContent,
+        code: lineContent,
         start: start,
         end: endIdx,
       };
@@ -88,76 +72,65 @@ function BlockInner(props: FileBlockProps) {
 
     setStart(undefined);
     setEnd(undefined);
-  }, [start, end, setExplanations, data, setStart, setEnd]);
-
-  const borderColor = `rgba(0, 0, 0, .1)`;
+  }, [start, end, setExplanations, content, setStart, setEnd]);
 
   return (
     <div className="py-4">
       <div className="max-w-6xl px-8 mx-auto overflow-hidden">
-        <div style={{ border: `1px solid ${borderColor}` }}>
-          {status === "loading" && <LoadingState content={content} />}
-          {status === "success" && data && (
-            <div className="grid grid-cols-[1fr_300px] relative">
-              {start && end && (
-                <div
-                  className="z-10 absolute"
-                  style={{ top: (start - 1) * 20 - 4, left: -15 }}
-                >
-                  <LineMenu onExplain={handleExplain} />
-                </div>
-              )}
-
-              <div className="min-w-0 overflow-auto">
-                {data.map((token, index) => {
-                  const lineNum = index + 1;
-                  const isHighlighted =
-                    start && end ? lineNum >= start && lineNum <= end : false;
-                  const hasLineMenu = isHighlighted && start === lineNum;
-
-                  return (
-                    <Line
-                      number={lineNum}
-                      isHighlighted={isHighlighted}
-                      hasLineMenu={hasLineMenu}
-                      token={token}
-                      key={index}
-                      onSelect={handleLineClick}
-                    />
-                  );
-                })}
-              </div>
-              <div>
-                <div
-                  className="grid h-full"
-                  style={{
-                    gridTemplateRows: `repeat(${data.length}, 20px)`,
-                  }}
-                >
-                  {Object.entries(explanations).map(([line, explanation]) => {
-                    return (
-                      <div
-                        key={line}
-                        className="pl-3 pr-2 overflow-y-auto"
-                        style={{
-                          boxShadow: `-2px 0 0 0 #539bf5, inset 1px 0 0 0 #539bf5`,
-                          gridColumn: 1,
-                          gridRow: `${explanation.start}/${
-                            explanation.end + 1
-                          }`,
-                        }}
-                      >
-                        <ExplanationComponent
-                          language={language}
-                          explanation={explanation}
-                        />
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
+        <div className="border relative">
+          {start && end && (
+            <div
+              className="z-20 absolute"
+              style={{ top: (start - 1) * 21 - 2, left: -20 }}
+            >
+              <LineMenu onExplain={handleExplain} />
             </div>
           )}
+          {Object.entries(explanations).map(([line, explanation]) => {
+            return (
+              <div
+                key={line}
+                className="px-3 overflow-y-auto absolute bg-white"
+                style={{
+                  top: (explanation.start - 1) * 21 - 2,
+                  height: (explanation.end - explanation.start + 1) * 21,
+                  right: 0,
+                  boxShadow: `-2px 0 0 0 #539bf5, inset 1px 0 0 0 #539bf5`,
+                  gridColumn: 1,
+                  gridRow: `${explanation.start}/${explanation.end + 1}`,
+                }}
+              >
+                <ExplanationComponent
+                  language={language}
+                  explanation={explanation}
+                />
+              </div>
+            );
+          })}
+          <SyntaxHighlighter
+            language={syntaxHighlighterLanguageMap[language] || "javascript"}
+            useInlineStyles={false}
+            wrapLines
+            className="!bg-transparent syntax-highlighter-block"
+            lineProps={(lineNumber) => {
+              const isHighlighted =
+                start && end && lineNumber >= start && lineNumber <= end;
+              return {
+                "data-highlighted": isHighlighted,
+                onClick: (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+                  let clicked = e.target as HTMLElement;
+                  if (!clicked.classList.contains("comment")) return;
+                  handleLineClick(lineNumber, e.shiftKey);
+                },
+                style: {
+                  display: "block",
+                },
+              };
+            }}
+            showLineNumbers
+          >
+            {content}
+          </SyntaxHighlighter>
         </div>
       </div>
     </div>
