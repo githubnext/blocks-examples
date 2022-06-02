ac93210fc0000796b7aa637d7595268f8c03c4fb
Amelia Wattenberger <wattenberger@github.com>
2022-06-02 15:08:27 -0700

    flat block - support yaml files

--- blocks/file-blocks/flat.tsx
+++ blocks/file-blocks/flat.tsx
@@ -2,30 +2,16 @@ import { tw } from "twind";
 import { FileBlockProps } from "@githubnext/blocks";
 import { Grid } from "@githubocto/flat-ui";
 import { csvFormat, csvParseRows } from "d3";
+import { load } from "js-yaml";
 import { useMemo } from "react";
 
-function parseCSV(content: string) {
-  try {
-    const rows = csvParseRows(content);
-    const headers = rows[0];
-    const csvData = rows.slice(1).map((row: any) => {
-      return headers.reduce((acc: Record<string, any>, key, i) => {
-        acc[key] = Number.isFinite(row[i]) ? +row[i] : row[i];
-        return acc;
-      }, {});
-    });
-    return csvData;
-  } catch (e) {
-    return [];
-  }
-}
-
 export default function (props: FileBlockProps) {
-  const { content, originalContent, isEditable, onUpdateContent } = props;
+  const { content, context, originalContent, isEditable, onUpdateContent } =
+    props;
 
-  const data = useMemo(() => parseCSV(content), [content]);
+  const data = useMemo(() => parseContent(context.path, content), [content]);
   const originalData = useMemo(
-    () => parseCSV(originalContent),
+    () => parseContent(context.path, originalContent),
     [originalContent]
   );
 
@@ -42,4 +28,52 @@ export default function (props: FileBlockProps) {
       />
     </div>
   );
+}
+
+function parseCSV(content: string): any[] {
+  try {
+    const rows = csvParseRows(content);
+    const headers = rows[0];
+    const csvData = rows.slice(1).map((row: any) => {
+      return headers.reduce((acc: Record<string, any>, key, i) => {
+        acc[key] = Number.isFinite(row[i]) ? +row[i] : row[i];
+        return acc;
+      }, {});
+    });
+    return csvData;
+  } catch (e) {
+    return [];
+  }
+}
+function parseYML(content: string): any[] {
+  try {
+    const data = load(content);
+    const isArray = Array.isArray(data);
+    if (isArray) return data;
+    // can we flatten an object into something sensical?
+    const flattenedData = Object.keys(data).map((key) => {
+      const contents = data[key];
+      const expandedContents =
+        typeof contents === "object" && contents !== null
+          ? contents
+          : { contents: JSON.stringify(contents) };
+      return {
+        __KEY__: key,
+        ...expandedContents,
+      };
+    });
+    return flattenedData;
+  } catch (e) {
+    return [];
+  }
+}
+function parseContent(path: string, content: string): any[] {
+  const extension = path.split(".").pop();
+  if (extension === "csv") {
+    return parseCSV(content);
+  } else if (["yaml", "yml"].includes(extension)) {
+    return parseYML(content);
+  } else {
+    return [];
+  }
 }
