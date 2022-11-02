3b3d497af4bdd7ceaff310474e7a267b8cbe3192
Amelia Wattenberger <wattenberger@github.com>
2022-11-02 16:34:45 -0700

    snazz up charts block

--- blocks/file-blocks/charts/Chart.tsx
+++ blocks/file-blocks/charts/Chart.tsx
@@ -20,6 +20,7 @@ import {
 } from "recharts";
 
 const styleName = "__style__";
+const dateTimeMetrics = ["date", "time", "datetime"];
 export function Chart({
   data,
   xMetric,
@@ -31,6 +32,7 @@ export function Chart({
   yMetric: string;
   type: "area" | "line" | "bar" | "scatter" | "pie";
 }) {
+  let dateMetrics = new Set();
   const sortedData = [...data]
     .sort((a, b) => a[xMetric] - b[xMetric])
     .map((d) => ({
@@ -39,9 +41,24 @@ export function Chart({
       [yMetric]: Number.isFinite(+d[yMetric]) ? +d[yMetric] : d[yMetric],
       style: null,
       [styleName]: d[styleName] || d["style"],
+      ...dateTimeMetrics.reduce((acc, m) => {
+        if (d[m]) {
+          let date = new Date(d[m]);
+          if (date.toString() === "Invalid Date") date = new Date(+d[m]);
+          if (date.toString() !== "Invalid Date") {
+            acc[m] = +date;
+            dateMetrics.add(m);
+          }
+        }
+        return acc;
+      }, {} as Record<string, Date>),
     }));
-  const parsedXMetric = xMetric === "style" ? styleName : xMetric;
-  const parsedYMetric = yMetric === "style" ? styleName : yMetric;
+  const parseMetric = (metric: string) => {
+    if (metric === "style") return styleName;
+    return metric;
+  };
+  const parsedXMetric = parseMetric(xMetric);
+  const parsedYMetric = parseMetric(yMetric);
 
   const ChartComponent = {
     area: AreaChartInners,
@@ -52,12 +69,17 @@ export function Chart({
   }[type];
   if (!ChartComponent) return null;
 
+  const formatXTick = tickFormatter(dateMetrics.has(parsedXMetric));
+  const formatYTick = tickFormatter(dateMetrics.has(parsedYMetric));
+
   return (
-    <div className={tw(`w-full h-full`)}>
+    <div className={tw(`w-full flex justify-center h-full`)}>
       <ChartComponent
         data={sortedData}
         xMetric={parsedXMetric}
         yMetric={parsedYMetric}
+        formatXTick={formatXTick}
+        formatYTick={formatYTick}
       />
     </div>
   );
@@ -67,35 +89,99 @@ type ChartProps = {
   data: Record<string, any>[];
   xMetric: string;
   yMetric: string;
+  formatXTick: (value: any) => string;
+  formatYTick: (value: any) => string;
 };
 
-const AreaChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
+const AreaChartInners = ({
+  data,
+  xMetric,
+  yMetric,
+  formatXTick,
+  formatYTick,
+}: ChartProps) => {
   return (
-    <ResponsiveContainer width="100%" height={500}>
+    <ResponsiveContainer width="100%" height={500} className={tw("font-mono")}>
       <AreaChart
         data={data}
         margin={{ top: 10, right: 30, left: 0, bottom: 30 }}
       >
         <defs>
           <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            <stop offset="5%" stopColor="#939AFF" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#939AFF" stopOpacity={0} />
           </linearGradient>
           <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
             <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
             <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
           </linearGradient>
         </defs>
-        <XAxis dataKey={xMetric}>
-          <Label value={xMetric} position="bottom" />
+        <CartesianGrid strokeDasharray="3 6" vertical={false} />
+        <XAxis
+          dataKey={xMetric}
+          tickLine={false}
+          tickCount={Math.floor(500 / 100)}
+          type="number"
+          domain={["min", "max"]}
+          tickFormatter={formatXTick}
+        >
+          <Label
+            value={xMetric}
+            position="bottom"
+            className={tw("font-mono")}
+          />
         </XAxis>
-        <YAxis dataKey={yMetric} />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
+        <YAxis
+          dataKey={yMetric}
+          tickLine={false}
+          tickCount={Math.floor(500 / 100)}
+          type="number"
+          domain={["auto", "auto"]}
+          tickFormatter={formatYTick}
+        >
+          <Label
+            value={yMetric}
+            position="insideTopLeft"
+            offset={20}
+            content={
+              <>
+                <text
+                  x={66}
+                  y={0}
+                  dy={17}
+                  className={tw("font-mono")}
+                  textAnchor="start"
+                  stroke="#fff"
+                  stroke-width={5}
+                >
+                  {yMetric}
+                </text>
+                <text
+                  x={66}
+                  y={0}
+                  dy={17}
+                  className={tw("font-mono")}
+                  textAnchor="start"
+                  fill="#000"
+                >
+                  {yMetric}
+                </text>
+              </>
+            }
+          />
+        </YAxis>
+        <Tooltip
+          content={
+            <TooltipContent
+              {...{ data, xMetric, yMetric, formatXTick, formatYTick }}
+            />
+          }
+        />
         <Area
           type="monotone"
           dataKey={yMetric}
-          stroke="#8884d8"
+          stroke="#939AFF"
+          strokeWidth={2}
           fillOpacity={1}
           fill="url(#colorUv)"
         />
@@ -103,57 +189,250 @@ const AreaChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
     </ResponsiveContainer>
   );
 };
-const LineChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
+const LineChartInners = ({
+  data,
+  xMetric,
+  yMetric,
+  formatXTick,
+  formatYTick,
+}: ChartProps) => {
   return (
     <ResponsiveContainer width="100%" height={500}>
       <LineChart
         data={data}
         margin={{ top: 10, right: 30, left: 0, bottom: 30 }}
       >
-        <XAxis dataKey={xMetric}>
-          <Label value={xMetric} position="bottom" />
+        <CartesianGrid strokeDasharray="3 6" />
+        <XAxis
+          dataKey={xMetric}
+          tickLine={false}
+          tickCount={Math.floor(500 / 100)}
+          type="number"
+          domain={["min", "max"]}
+          tickFormatter={formatXTick}
+        >
+          <Label
+            value={xMetric}
+            position="bottom"
+            className={tw("font-mono")}
+          />
         </XAxis>
-        <YAxis dataKey={yMetric} />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Line type="monotone" dataKey={yMetric} stroke="#8884d8" />
+        <YAxis
+          dataKey={yMetric}
+          tickLine={false}
+          tickCount={Math.floor(500 / 100)}
+          type="number"
+          domain={["auto", "auto"]}
+          tickFormatter={formatYTick}
+        >
+          <Label
+            value={yMetric}
+            position="insideTopLeft"
+            offset={20}
+            content={
+              <>
+                <text
+                  x={66}
+                  y={0}
+                  dy={17}
+                  className={tw("font-mono")}
+                  textAnchor="start"
+                  stroke="#fff"
+                  stroke-width={5}
+                >
+                  {yMetric}
+                </text>
+                <text
+                  x={66}
+                  y={0}
+                  dy={17}
+                  className={tw("font-mono")}
+                  textAnchor="start"
+                  fill="#000"
+                >
+                  {yMetric}
+                </text>
+              </>
+            }
+          />
+        </YAxis>
+        <Tooltip
+          content={
+            <TooltipContent
+              {...{ data, xMetric, yMetric, formatXTick, formatYTick }}
+            />
+          }
+        />
+        <Line type="monotone" dataKey={yMetric} stroke="#939AFF" />
       </LineChart>
     </ResponsiveContainer>
   );
 };
-const BarChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
+const BarChartInners = ({
+  data,
+  xMetric,
+  yMetric,
+  formatXTick,
+  formatYTick,
+}: ChartProps) => {
   return (
     <ResponsiveContainer width="100%" height={500}>
       <BarChart
         data={data}
         margin={{ top: 10, right: 30, left: 0, bottom: 30 }}
       >
-        <XAxis dataKey={xMetric}>
-          <Label value={xMetric} position="bottom" />
+        <CartesianGrid strokeDasharray="3 6" />
+        <XAxis
+          dataKey={xMetric}
+          tickLine={false}
+          tickCount={Math.floor(500 / 100)}
+          type="number"
+          domain={["min", "max"]}
+          tickFormatter={formatXTick}
+        >
+          <Label
+            value={xMetric}
+            position="bottom"
+            className={tw("font-mono")}
+          />
         </XAxis>
-        <YAxis dataKey={yMetric} />
-        <CartesianGrid strokeDasharray="3 3" />
-        <Tooltip />
-        <Bar dataKey={yMetric} fill="#8884d8" />
+        <YAxis
+          dataKey={yMetric}
+          tickLine={false}
+          tickCount={Math.floor(500 / 100)}
+          type="number"
+          domain={["auto", "auto"]}
+          tickFormatter={formatYTick}
+        >
+          <Label
+            value={yMetric}
+            position="insideTopLeft"
+            offset={20}
+            content={
+              <>
+                <text
+                  x={66}
+                  y={0}
+                  dy={17}
+                  className={tw("font-mono")}
+                  textAnchor="start"
+                  stroke="#fff"
+                  stroke-width={5}
+                >
+                  {yMetric}
+                </text>
+                <text
+                  x={66}
+                  y={0}
+                  dy={17}
+                  className={tw("font-mono")}
+                  textAnchor="start"
+                  fill="#000"
+                >
+                  {yMetric}
+                </text>
+              </>
+            }
+          />
+        </YAxis>
+        <Tooltip
+          content={
+            <TooltipContent
+              {...{ data, xMetric, yMetric, formatXTick, formatYTick }}
+            />
+          }
+        />
+        <Bar dataKey={yMetric} fill="#939AFF" />
       </BarChart>
     </ResponsiveContainer>
   );
 };
-const ScatterChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
+const ScatterChartInners = ({
+  data,
+  xMetric,
+  yMetric,
+  formatXTick,
+  formatYTick,
+}: ChartProps) => {
   return (
     <ScatterChart
       width={500}
       height={500}
       margin={{ top: 10, right: 30, left: 0, bottom: 30 }}
     >
-      <XAxis dataKey={xMetric}>
-        <Label value={xMetric} position="bottom" />
+      <CartesianGrid strokeDasharray="3 6" />
+      <XAxis
+        dataKey={xMetric}
+        tickLine={false}
+        tickCount={Math.floor(500 / 150)}
+        type="number"
+        domain={["auto", "auto"]}
+        tickFormatter={formatXTick}
+      >
+        <Label value={xMetric} position="bottom" className={tw("font-mono")} />
       </XAxis>
-      <YAxis dataKey={yMetric} />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip />
-      <Scatter name="items" data={data} fill="#8884d8" />
+      <YAxis
+        dataKey={yMetric}
+        tickLine={false}
+        tickCount={Math.floor(500 / 100)}
+        type="number"
+        domain={["auto", "auto"]}
+        tickFormatter={formatYTick}
+      >
+        <Label
+          value={yMetric}
+          position="insideTopLeft"
+          offset={20}
+          content={
+            <>
+              <text
+                x={66}
+                y={0}
+                dy={17}
+                className={tw("font-mono")}
+                textAnchor="start"
+                stroke="#fff"
+                stroke-width={5}
+              >
+                {yMetric}
+              </text>
+              <text
+                x={66}
+                y={0}
+                dy={17}
+                className={tw("font-mono")}
+                textAnchor="start"
+                fill="#000"
+              >
+                {yMetric}
+              </text>
+            </>
+          }
+        />
+      </YAxis>
+      <Tooltip
+        content={
+          <TooltipContent
+            {...{ data, xMetric, yMetric, formatXTick, formatYTick }}
+          />
+        }
+      />
+      <Scatter
+        name="items"
+        data={data}
+        fill="#939AFF"
+        shape={<ScatterPoint {...{ xMetric, yMetric }} />}
+      />
     </ScatterChart>
+  );
+};
+
+const ScatterPoint = ({ xMetric, yMetric, cx, cy, payload }: any) => {
+  const { x, y } = payload;
+  return (
+    <g transform={`translate(${cx},${cy})`}>
+      <circle r={5} fill="#939AFFcc" />
+    </g>
   );
 };
 
@@ -171,7 +450,13 @@ const COLORS = [
   "#4b4b8f",
   "#A5AA99",
 ];
-const PieChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
+const PieChartInners = ({
+  data,
+  xMetric,
+  yMetric,
+  formatXTick,
+  formatYTick,
+}: ChartProps) => {
   const parsedData = data.map((d) => ({
     ...d,
     value: +d[xMetric],
@@ -182,7 +467,7 @@ const PieChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
         data={parsedData}
         dataKey="value"
         nameKey={yMetric}
-        fill="#8884d8"
+        fill="#939AFF"
         label={(d) => d[yMetric]}
         labelLine={false}
       >
@@ -191,7 +476,41 @@ const PieChartInners = ({ data, xMetric, yMetric }: ChartProps) => {
         ))}
       </Pie>
 
-      <Tooltip />
+      <Tooltip
+        content={
+          <TooltipContent
+            {...{ data, xMetric, yMetric, formatXTick, formatYTick }}
+          />
+        }
+      />
     </PieChart>
   );
+};
+
+const TooltipContent = ({
+  xMetric,
+  yMetric,
+  formatXTick,
+  formatYTick,
+  payload,
+}: { payload: any } & ChartProps) => {
+  const d = payload?.[0]?.payload;
+  if (!d) return null;
+  return (
+    <div className={tw("bg-white py-2 px-3 shadow-lg")}>
+      <p>
+        <strong>{xMetric}</strong>:{" "}
+        <span className={tw("font-mono")}>{formatXTick(d[xMetric])}</span>
+      </p>
+      <p>
+        <strong>{yMetric}</strong>:{" "}
+        <span className={tw("font-mono")}>{formatYTick(d[yMetric])}</span>
+      </p>
+    </div>
+  );
+};
+
+const tickFormatter = (isDate: boolean) => (tick: any) => {
+  if (isDate) return new Date(tick).toLocaleDateString?.() || tick;
+  return typeof tick === "number" ? tick.toLocaleString() : tick;
 };
--- blocks/file-blocks/charts/index.tsx
+++ blocks/file-blocks/charts/index.tsx
@@ -1,6 +1,12 @@
 import { tw } from "twind";
 import { FileBlockProps } from "@githubnext/blocks";
-import { ActionList, ActionMenu, Text } from "@primer/react";
+import {
+  ActionList,
+  ActionMenu,
+  IconButton,
+  Text,
+  TextInput,
+} from "@primer/react";
 import { parse } from "papaparse";
 import { useEffect, useMemo, useState } from "react";
 // @ts-ignore: we need to specify the file extension
@@ -8,9 +14,18 @@ import { Chart } from "./Chart.tsx";
 // @ts-ignore: we need to specify the file extension
 import { ErrorBoundary } from "./ErrorBoundary.tsx";
 import { Button } from "@primer/react";
-import { CheckIcon } from "@primer/octicons-react";
+import {
+  CheckIcon,
+  PlusIcon,
+  SearchIcon,
+  TrashIcon,
+} from "@primer/octicons-react";
 
 export default function (props: FileBlockProps) {
+  return <Wrapper {...props} />;
+}
+
+function Wrapper(props: FileBlockProps) {
   const { content, metadata, onUpdateMetadata } = props;
   const [xMetric, setXMetric] = useState("");
   const [yMetric, setYMetric] = useState("");
@@ -49,69 +64,115 @@ export default function (props: FileBlockProps) {
   }, [keys.join(","), savedChartConfigs.join(",")]);
 
   return (
-    <div className={tw(`w-full h-full`)}>
-      <div className={tw(`flex p-3`)}>
-        <Select
-          label="saved charts"
-          value={activeChartConfig}
-          onChange={onLoadChartConfig}
-          options={savedChartConfigs}
-        />
-        {activeChartConfigIndex !== -1 ? (
-          <Button
-            variant="danger"
-            className={tw(`ml-2`)}
-            onClick={() => {
-              const newMetadata = {
-                configs: savedChartConfigs.filter(
-                  (c: string) => c !== activeChartConfig
-                ),
-              };
-              onUpdateMetadata(newMetadata);
-            }}
-          >
-            Delete config
-          </Button>
-        ) : (
-          <Button
-            className={tw(`ml-2`)}
-            onClick={() => {
-              const newMetadata = {
-                configs: [...savedChartConfigs, activeChartConfig],
-              };
-              onUpdateMetadata(newMetadata);
-            }}
-          >
-            Save config
-          </Button>
-        )}
-        <div className={tw(`ml-auto flex items-center flex-wrap`)}>
-          <div className={tw(`mr-2`)}>
+    <div className={tw(`w-full h-full p-6`)}>
+      <div className={tw(`w-full flex`)}>
+        <div className={tw(`flex-none w-[20em]`)}>
+          <div className={tw(`m-6 space-y-2`)}>
+            <h6
+              className={tw(`text-sm uppercase text-gray-500 tracking-widest`)}
+            >
+              Metrics
+            </h6>
             <Select
-              label="x metric"
+              label="x"
               value={xMetric}
               onChange={setXMetric}
               options={keys}
             />
-          </div>
-          <div className={tw(`mr-2`)}>
             <Select
-              label="y metric"
+              label="y"
               value={yMetric}
               onChange={setYMetric}
               options={keys}
             />
+            <Select
+              label="type"
+              value={chartType}
+              onChange={setChartType}
+              options={chartTypes}
+            />
           </div>
-          <Select
-            label="chart type"
-            value={chartType}
-            onChange={setChartType}
-            options={chartTypes}
-          />
-        </div>
-      </div>
 
-      <div className={tw(`w-full`)}>
+          <h6
+            className={tw(
+              `text-sm ml-6 mt-16 uppercase text-gray-500 tracking-widest`
+            )}
+          >
+            Saved charts
+          </h6>
+
+          <ActionList showDividers selectionVariant="single">
+            {savedChartConfigs.map((chartConfig, index) => {
+              const [xMetric, yMetric, chartType] = chartConfig.split(", ");
+              return (
+                <ActionList.Item
+                  className={tw("items-center")}
+                  key={index}
+                  selected={activeChartConfigIndex === index}
+                  onClick={() => onLoadChartConfig(chartConfig)}
+                >
+                  <div className={tw(`py-1`)}>
+                    <span className={tw("font-bold")}>{xMetric}</span>
+                    <span className={tw("mx-1")}>vs</span>
+                    <span className={tw("font-bold")}>{yMetric}</span>
+                    <ActionList.Description>{chartType}</ActionList.Description>
+                  </div>
+                  {activeChartConfigIndex === index && (
+                    <ActionList.TrailingVisual>
+                      <IconButton
+                        icon={TrashIcon}
+                        size="small"
+                        variant="danger"
+                        className={tw(`ml-2`)}
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          const newMetadata = {
+                            configs: savedChartConfigs.filter(
+                              (c: string) => c !== activeChartConfig
+                            ),
+                          };
+                          onUpdateMetadata(newMetadata);
+                        }}
+                      >
+                        Delete
+                      </IconButton>
+                    </ActionList.TrailingVisual>
+                  )}
+                </ActionList.Item>
+              );
+            })}
+            {!savedChartConfigs.find(
+              (c: string) => c === activeChartConfig
+            ) && (
+              <ActionList.Item
+                className={tw("items-center")}
+                selected
+                onClick={() => {
+                  const newMetadata = {
+                    configs: [...savedChartConfigs, activeChartConfig],
+                  };
+                  onUpdateMetadata(newMetadata);
+                }}
+              >
+                <div className={tw(`py-1`)}>
+                  <span className={tw("font-bold")}>{xMetric}</span>
+                  <span className={tw("mx-1")}>vs</span>
+                  <span className={tw("font-bold")}>{yMetric}</span>
+                  <ActionList.Description>{chartType}</ActionList.Description>
+                </div>
+                <ActionList.TrailingVisual>
+                  <IconButton
+                    icon={PlusIcon}
+                    size="small"
+                    className={tw(`ml-2`)}
+                  >
+                    Save
+                  </IconButton>
+                </ActionList.TrailingVisual>
+              </ActionList.Item>
+            )}
+          </ActionList>
+        </div>
         <ErrorBoundary>
           <Chart
             data={data}
@@ -138,37 +199,63 @@ const Select = ({
   options: string[];
   onChange: (value: string) => void;
 }) => {
+  const [search, setSearch] = useState("");
   return (
-    <ActionMenu>
-      <ActionMenu.Button>
-        {label}: {value}
-      </ActionMenu.Button>
-      <ActionMenu.Overlay className={tw("max-h-[80vh]")}>
-        {options.length ? (
-          <ActionList>
-            {options.map((option) => (
-              <ActionList.Item key={option} onSelect={() => onChange(option)}>
-                <ActionList.LeadingVisual>
-                  {option === value ? <CheckIcon /> : null}
-                </ActionList.LeadingVisual>
-                {option}
-              </ActionList.Item>
-            ))}
-          </ActionList>
-        ) : (
-          <Text
-            color="fg.muted"
-            textAlign="center"
-            fontStyle="italic"
-            display="block"
-            mx="auto"
-            my="2"
-          >
-            No options
-          </Text>
-        )}
-      </ActionMenu.Overlay>
-    </ActionMenu>
+    <div className={tw(`flex items-center`)}>
+      <div className={tw(`font-mono text-sm text-gray-500 w-16`)}>{label}</div>
+      <ActionMenu>
+        <ActionMenu.Button>{value}</ActionMenu.Button>
+        <ActionMenu.Overlay>
+          {options.length ? (
+            <ActionList sx={{ pb: 0 }}>
+              <div className={tw(`p-2 pt-0`)}>
+                <TextInput
+                  leadingVisual={SearchIcon}
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                  autoFocus
+                  className={tw(`w-full`)}
+                />
+              </div>
+              <div
+                className={tw(
+                  "relative max-h-[50vh] overflow-y-auto pb-2 z-10"
+                )}
+              >
+                {options.map((option) => {
+                  if (!option.toLowerCase().includes(search.toLowerCase()))
+                    return null;
+                  return (
+                    <ActionList.Item
+                      key={option}
+                      onSelect={() => onChange(option)}
+                    >
+                      <ActionList.LeadingVisual>
+                        {option === value ? <CheckIcon /> : null}
+                      </ActionList.LeadingVisual>
+                      {option}
+                    </ActionList.Item>
+                  );
+                })}
+              </div>
+            </ActionList>
+          ) : (
+            <Text
+              color="fg.muted"
+              textAlign="center"
+              fontStyle="italic"
+              display="block"
+              mx="auto"
+              my="2"
+            >
+              No options
+            </Text>
+          )}
+        </ActionMenu.Overlay>
+      </ActionMenu>
+    </div>
   );
 };
 
