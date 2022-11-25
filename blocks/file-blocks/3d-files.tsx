4b50e7181ff47d3430382c56a8ad50b96fde20f3
— <drcmda@gmail.com>
2022-11-25 10:01:10 +0100

    fix: 3d file block uses stage component, update r3f and drei

--- blocks/file-blocks/3d-files.tsx
+++ blocks/file-blocks/3d-files.tsx
@@ -1,48 +1,32 @@
-import { Suspense, useRef, useState, useEffect } from "react";
-import { tw } from "twind";
-import { PerspectiveCamera, useGLTF } from "@react-three/drei";
-import { Canvas, useLoader, useStore } from "@react-three/fiber";
+import { Suspense, useLayoutEffect } from "react";
+import { Stage, useGLTF } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { FileBlockProps } from "@githubnext/blocks";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-const LControl = () => {
-  // @ts-ignore
-  const dom = useStore((state) => state.dom);
-  const control = useRef(null);
-
-  // @ts-ignore
-  return <OrbitControls ref={control} domElement={dom.current} />;
-};
 
 function Model({ url }: { url: string }) {
-  // need to load gltf model this way instead of with hook
-  // or you get an infinite loop on the production sandbox side
-  const { scene } = useLoader(GLTFLoader, url);
+  const { scene } = useGLTF(url);
+  useLayoutEffect(() => {
+    scene.traverse((object) => {
+      if (object.type === "Mesh") {
+        object.receiveShadow = object.castShadow = true;
+      }
+    });
+  }, []);
   return <primitive object={scene} dispose={null} />;
 }
 
 export default function (props: FileBlockProps) {
   const { context } = props;
-
   const url = `https://raw.githubusercontent.com/${context.owner}/${context.repo}/${context.sha}/${context.path}`;
-
   return (
-    <Canvas style={{ height: "100vh" }}>
-      {/*
-        // @ts-ignore */}
-      <PerspectiveCamera
-        makeDefault
-        position={[0, 0, 0.3]}
-        near={0.01}
-        far={1000}
-      />
-      <ambientLight />
-      <LControl />
-      <pointLight position={[10, 10, 10]} />
+    <Canvas shadows style={{ height: "100vh" }}>
       <Suspense fallback={null}>
-        <Model url={url} />
+        <Stage shadows="accumulative">
+          <Model url={url} />
+        </Stage>
       </Suspense>
+      <OrbitControls makeDefault />
     </Canvas>
   );
 }
