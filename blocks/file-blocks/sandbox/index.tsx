3f1e9d5c40b337019ae17c4267c71114629c9eab
Danilo Woznica <danilowoz@gmail.com>
2022-11-29 11:07:17 +0000

    load file

--- blocks/file-blocks/sandbox/index.tsx
+++ blocks/file-blocks/sandbox/index.tsx
@@ -1,74 +1,27 @@
 import { tw } from "twind";
-import { ReactNode, useEffect, useState } from "react";
-import LZString from "lz-string";
 import { FileBlockProps } from "@githubnext/blocks";
 import { Sandpack } from "@codesandbox/sandpack-react";
 import { githubLight } from "@codesandbox/sandpack-themes";
 
-const optionsDefaults = {
-  fontsize: 14,
-  hidenavigation: 1,
-  codemirror: 1,
-  hidedevtools: 0,
-  expanddevtools: 1,
-  view: "split",
-};
+export default ({ content }: FileBlockProps) => {
+  const files = { "src/index.js": content };
 
-export default ({ content, ...props }: FileBlockProps) => {
-  const files = { "index.js": content };
   return (
     <Sandpack
+      template="vanilla"
       files={files}
       theme={githubLight}
-      options={{ editorHeight: 292 }}
+      customSetup={{ dependencies: getDependenciesFromString(content) }}
+      options={{
+        editorHeight: 300,
+        showConsole: content.includes("console."),
+        showTabs: true,
+        classes: {
+          "sp-layout": tw("border-0", "bg-gray-200"),
+          "sp-button": tw("bg-gray-100"),
+        },
+      }}
     />
-  );
-
-  const [url, setUrl] = useState("");
-  const parameters = getParameters({
-    files: {
-      "index.js": {
-        // @ts-ignore
-        content,
-        isBinary: false,
-      },
-      "package.json": {
-        content: JSON.stringify({
-          dependencies: getDependenciesFromString(content),
-        }),
-        isBinary: false,
-      },
-    },
-  });
-
-  const getSandboxUrl = async () => {
-    const url = `https://codesandbox.io/api/v1/sandboxes/define?parameters=${parameters}&json=1`;
-    const res = await fetch(url);
-    const data = await res.json();
-    const id = data?.sandbox_id;
-    const params = new URLSearchParams({
-      ...optionsDefaults,
-      ...(state.sandboxOptions || {}),
-    }).toString();
-    const iframeUrl = `https://codesandbox.io/embed/${id}?${params}`;
-
-    setUrl(iframeUrl);
-  };
-  useEffect(() => {
-    getSandboxUrl();
-  }, []);
-
-  return (
-    <div className={tw(`w-full h-full`)}>
-      {!!url && (
-        <iframe
-          className={tw(`w-full h-full outline-none`)}
-          src={url}
-          title="CodeSandbox"
-          sandbox="allow-modals allow-forms allow-popups allow-scripts allow-same-origin"
-        />
-      )}
-    </div>
   );
 };
 
@@ -88,14 +41,3 @@ const getDependenciesFromString = (content: string) => {
   });
   return dependencies;
 };
-
-// ported from "codesandbox/lib/api/define"
-function compress(input: string) {
-  return LZString.compressToBase64(input)
-    .replace(/\+/g, "-") // Convert '+' to '-'
-    .replace(/\//g, "_") // Convert '/' to '_'
-    .replace(/=+$/, ""); // Remove ending '='
-}
-function getParameters(parameters: any) {
-  return compress(JSON.stringify(parameters));
-}
